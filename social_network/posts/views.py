c8d8c07bdec7707c4e12fc98c9ec9fc6933257bc
jeSoosRemirez <dkhomych0091@gmail.com>
2021-12-12 16:37:43 +0200

    Create views and api_view for posts

--- social_network/posts/views.py
+++ social_network/posts/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from posts.models import Post
+from posts.serializers import PostListSerializer, PostDetailSerializer
 
-# Create your views here.
+
+class PostListView(ListCreateAPIView):
+    """
+    This APIView provides `list` action
+    with 'author', 'author_email', 'text', 'created_time'.
+    """
+    queryset = Post.objects.all()
+    serializer_class = PostListSerializer
+
+
+class PostDetailView(RetrieveUpdateDestroyAPIView):
+    """
+    This APIView provides `list` action
+    with 'id', 'author', 'author_email', 'text', 'created_time'.
+    """
+    queryset = Post.objects.all()
+    serializer_class = PostDetailSerializer
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'posts': reverse('post-list', request=request, format=format),
+    })
