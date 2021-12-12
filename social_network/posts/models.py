14645b449692694cfbba7f1bf95c8e796f33a96d
jeSoosRemirez <dkhomych0091@gmail.com>
2021-12-12 16:36:47 +0200

    Create model Post for posts

--- social_network/posts/models.py
+++ social_network/posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    owner = models.ForeignKey('auth.User', related_name='social_network_app', on_delete=models.CASCADE)
+    text = models.TextField(max_length=100, blank=False)
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_time']
