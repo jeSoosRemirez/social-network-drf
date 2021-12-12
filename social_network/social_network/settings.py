faf60fefc77d54c7adb15cf8c84921a09a35331d
jeSoosRemirez <dkhomych0091@gmail.com>
2021-12-12 15:56:45 +0200

    Add psql db

--- social_network/social_network/settings.py
+++ social_network/social_network/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'social_network.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'social_network_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',    # Default HOST: localhost
+        'PORT': 5432,   # Default PORT: 5432
     }
 }
 
