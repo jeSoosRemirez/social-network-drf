032cc14ab05c5d2d69ef1a122edb655092dc856f
jeSoosRemirez <dkhomych0091@gmail.com>
2021-12-13 20:18:14 +0200

    Add api roots for users, add permission and authentication classes to views

--- social_network/posts/views.py
+++ social_network/posts/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from posts.models import Post
 from posts.serializers import PostListSerializer, PostDetailSerializer
+from users.backends import JWTAuthentication
 
 
 class PostListView(ListCreateAPIView):
     """
     This APIView provides `list` action
-    with 'author', 'author_email', 'text', 'created_time'.
+    with 'owner', 'author_email', 'text', 'created_time'.
     """
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (JWTAuthentication,)
     queryset = Post.objects.all()
     serializer_class = PostListSerializer
 
@@ -21,6 +25,8 @@ class PostDetailView(RetrieveUpdateDestroyAPIView):
     This APIView provides `list` action
     with 'id', 'author', 'author_email', 'text', 'created_time'.
     """
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (JWTAuthentication,)
     queryset = Post.objects.all()
     serializer_class = PostDetailSerializer
 
@@ -28,5 +34,11 @@ class PostDetailView(RetrieveUpdateDestroyAPIView):
 @api_view(['GET'])
 def api_root(request, format=None):
     return Response({
+        # Posts api roots
         'posts': reverse('post-list', request=request, format=format),
+
+        # Users api roots
+        'register': reverse('register', request=request, format=format),
+        'login': reverse('login', request=request, format=format),
+        'update': reverse('update', request=request, format=format),
     })
