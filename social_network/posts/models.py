c5257b11fe53e33a3c7653880e3351972508c0a6
jeSoosRemirez <dkhomych0091@gmail.com>
2021-12-13 20:13:08 +0200

    Change auth user model in owner

--- social_network/posts/models.py
+++ social_network/posts/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.conf import settings
 
 
 class Post(models.Model):
-    owner = models.ForeignKey('auth.User', related_name='social_network_app', on_delete=models.CASCADE)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='social_network_app', on_delete=models.CASCADE)
     text = models.TextField(max_length=100, blank=False)
     created_time = models.DateTimeField(auto_now_add=True)
 
