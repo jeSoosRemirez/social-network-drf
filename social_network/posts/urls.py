bce86ab2774cbfe2489b08ed98a436a767973b3d
jeSoosRemirez <dkhomych0091@gmail.com>
2021-12-15 19:34:22 +0200

    Add url for likes analytic

--- social_network/posts/urls.py
+++ social_network/posts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from posts.views import PostDetailView, PostListView, api_root, PostCreateView, PostLikeView
+from posts.views import PostDetailView, PostListView, api_root, PostCreateView, PostLikeView, AnalyticView
 from rest_framework.urlpatterns import format_suffix_patterns
 
 
@@ -10,4 +10,5 @@ urlpatterns = format_suffix_patterns([
     path('posts/create/', PostCreateView.as_view(), name='post-create'),
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('posts/<int:pk>/like/', PostLikeView.as_view()),
+    path('api/analytics/', AnalyticView.as_view()),
 ])
