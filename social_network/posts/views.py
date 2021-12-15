4989b63ee771f605da854511868424c5e46ec101
jeSoosRemirez <dkhomych0091@gmail.com>
2021-12-15 15:05:52 +0200

    Add views for liking and creating posts

--- social_network/posts/views.py
+++ social_network/posts/views.py
@@ -1,18 +1,34 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from rest_framework.decorators import api_view
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import RetrieveUpdateDestroyAPIView, CreateAPIView, ListAPIView, get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
+from rest_framework.views import APIView
 from posts.models import Post
-from posts.serializers import PostListSerializer, PostDetailSerializer
+from posts.serializers import PostListSerializer, PostDetailSerializer, PostCreateSerializer
 from users.backends import JWTAuthentication
 
 
-class PostListView(ListCreateAPIView):
+class PostCreateView(CreateAPIView):
+    """
+    This APIView provides `create` action
+    with 'owner', 'text', 'created_time'.
+    """
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (JWTAuthentication,)
+    queryset = Post.objects.all()
+    serializer_class = PostCreateSerializer
+
+    def perform_create(self, serializer):
+        # The request user is set as owner automatically.
+        serializer.save(owner=self.request.user)
+
+
+class PostListView(ListAPIView):
     """
     This APIView provides `list` action
-    with 'owner', 'author_email', 'text', 'created_time'.
+    with 'owner', 'text', 'likes', 'dislikes', 'created_time'.
     """
     permission_classes = (IsAuthenticated,)
     authentication_classes = (JWTAuthentication,)
@@ -23,7 +39,7 @@ class PostListView(ListCreateAPIView):
 class PostDetailView(RetrieveUpdateDestroyAPIView):
     """
     This APIView provides `list` action
-    with 'id', 'author', 'author_email', 'text', 'created_time'.
+    with 'id', 'owner', 'text', 'created_time'.
     """
     permission_classes = (IsAuthenticated,)
     authentication_classes = (JWTAuthentication,)
@@ -31,11 +47,27 @@ class PostDetailView(RetrieveUpdateDestroyAPIView):
     serializer_class = PostDetailSerializer
 
 
+class PostLikeView(APIView):
+
+    def get(self, request, pk):
+        if request.user.is_authenticated:
+            post = get_object_or_404(Post, id=pk)
+            if request.user in post.liked_by.all():
+                post.liked_by.remove(request.user)
+            else:
+                post.liked_by.add(request.user)
+            post.save()
+            return Response({'success': True})
+        else:
+            return Response({'success': False})
+
+
 @api_view(['GET'])
 def api_root(request, format=None):
     return Response({
         # Posts api roots
         'posts': reverse('post-list', request=request, format=format),
+        'post-create': reverse('post-create', request=request, format=format),
 
         # Users api roots
         'register': reverse('register', request=request, format=format),
