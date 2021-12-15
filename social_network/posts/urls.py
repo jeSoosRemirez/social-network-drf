04bf9f8ff8ba6af2debdac4fe9aa0c00fb0075c3
jeSoosRemirez <dkhomych0091@gmail.com>
2021-12-15 15:05:12 +0200

    Add urls for creating and liking post

--- social_network/posts/urls.py
+++ social_network/posts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from posts.views import PostDetailView, PostListView, api_root
+from posts.views import PostDetailView, PostListView, api_root, PostCreateView, PostLikeView
 from rest_framework.urlpatterns import format_suffix_patterns
 
 
@@ -7,5 +7,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = format_suffix_patterns([
     path('', api_root),
     path('posts/', PostListView.as_view(), name='post-list'),
+    path('posts/create/', PostCreateView.as_view(), name='post-create'),
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('posts/<int:pk>/like/', PostLikeView.as_view()),
 ])
