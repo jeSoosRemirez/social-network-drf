1e60d3c205698cbe3ef14cf8417194a760401cc9
jeSoosRemirez <dkhomych0091@gmail.com>
2021-12-15 15:03:47 +0200

    Add Like model. Add liked_by in Post model, change owner args

--- social_network/posts/models.py
+++ social_network/posts/models.py
@@ -1,10 +1,21 @@
 from django.db import models
 from django.conf import settings
+from users.models import User
 
 
 class Post(models.Model):
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='social_network_app', on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, related_name='owner', on_delete=models.CASCADE)
     text = models.TextField(max_length=100, blank=False)
+    created_time = models.DateTimeField(auto_now_add=True)
+    liked_by = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='liked', through='Like')
+
+    class Meta:
+        ordering = ['created_time']
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
     created_time = models.DateTimeField(auto_now_add=True)
 
     class Meta:
